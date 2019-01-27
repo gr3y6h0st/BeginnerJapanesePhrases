fcee2d96598f839dd53ac228ae22632c9e3ca874
gr3y6h0st <jayvy19@gmail.com>
2019-01-26 16:46:58 -0800

    Begun Adding GIF images for hiragana vowels a,i,u.

--- src/main/java/com/android/beginnerleveljapanese/utils/HiraganaAdapter.java
+++ src/main/java/com/android/beginnerleveljapanese/utils/HiraganaAdapter.java
@@ -3,18 +3,22 @@ package com.android.beginnerleveljapanese.utils;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.beginnerleveljapanese.R;
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
+
 
 public class HiraganaAdapter extends RecyclerView.Adapter<HiraganaAdapter.HiraganaViewHolder> {
 
@@ -40,11 +44,17 @@ public class HiraganaAdapter extends RecyclerView.Adapter<HiraganaAdapter.Hiraga
         @BindView(R.id.hiraganaTv)
         TextView hiraganaTv;
 
+        @BindView(R.id.hiragana_list_iv)
+        ImageView hirganaIv;
+
+        @BindView(R.id.hiragana_card_view)
+        CardView mCardView;
+
         HiraganaViewHolder(View view){
             super(view);
             ButterKnife.bind(this, view);
             view.setOnClickListener(this);
-
+            hirganaIv.setVisibility(View.GONE);
         }
 
         //handles click Events when users select a Phrase Category (records position of click).
@@ -70,7 +80,7 @@ public class HiraganaAdapter extends RecyclerView.Adapter<HiraganaAdapter.Hiraga
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HiraganaViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final HiraganaViewHolder holder, int position) {
         if(data == null) {
             Log.d(TAG, "data is null");
             return;
@@ -81,9 +91,85 @@ public class HiraganaAdapter extends RecyclerView.Adapter<HiraganaAdapter.Hiraga
             String hiragana_category_title = data[position];
             //populate each category tv w/ proper title.
             holder.hiraganaTv.setText(hiragana_category_title);
+
+
+            switch (hiragana_category_title){
+                case "\u3041":
+                    Glide.with(mContext)
+                            .asGif()
+                            .load(R.drawable.hiragana_a_stroke_order_animation)
+                            .into(holder.hirganaIv);
+                    break;
+
+
+                case "\u3042":
+                    Glide.with(mContext)
+                            .asGif()
+                            .load(R.drawable.hiragana_a_stroke_order_animation)
+                            .into(holder.hirganaIv);
+
+                case "\u3043":
+                    Glide.with(mContext)
+                            .load(R.drawable.hiragana_i_stroke_order_animation)
+                            .into(holder.hirganaIv);
+                    break;
+
+                case "\u3044":
+                    Glide.with(mContext)
+                            .asGif()
+                            .load(R.drawable.hiragana_i_stroke_order_animation)
+                            .into(holder.hirganaIv);
+                    break;
+
+                case "\\u3045":
+                    Glide.with(mContext)
+                            .asGif()
+                            .load(R.drawable.hiragana_u_stroke_order_animation)
+                            .into(holder.hirganaIv);
+                    break;
+
+                case "\\u3046":
+                    Glide.with(mContext)
+                            .asGif()
+                            .load(R.drawable.hiragana_u_stroke_order_animation)
+                            .into(holder.hirganaIv);
+                    break;
+                    /*
+
+                case "\\u3047":
+                    Glide.with(mContext)
+                            .load(R.drawable.arrow_down)
+                            .into(holder.hirganaIv);
+                    break;
+
+                case "\u3048":
+                    Glide.with(mContext)
+                            .asGif()
+                            .load(R.drawable.hiragana_a_stroke_order_animation)
+                            .into(holder.hirganaIv);
+                    break;
+
+                case "\u3049":
+                    Glide.with(mContext)
+                            .asGif()
+                            .load(R.drawable.hiragana_a_stroke_order_animation)
+                            .into(holder.hirganaIv);
+                    break;
+                    */
+            }
         }
 
-
+        //CardView listener expands/contracts CardView's Image.
+        holder.mCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (holder.hirganaIv.getVisibility() == View.GONE) {
+                    holder.hirganaIv.setVisibility(View.VISIBLE);
+                } else {
+                    holder.hirganaIv.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     @Override
