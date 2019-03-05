86b9fef2b53e13fdb0588022c89699de03bb06fa
gr3y6h0st <jayvy19@gmail.com>
2019-03-04 22:59:54 -0800

    Working on the Layout format for the Widget. Using LinearLayouts to display all text for now.

--- src/main/java/com/android/beginnerleveljapanese/widget/BeginnerJapaneseWidgetProvider.java
+++ src/main/java/com/android/beginnerleveljapanese/widget/BeginnerJapaneseWidgetProvider.java
@@ -6,6 +6,8 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
 
 import com.android.beginnerleveljapanese.MainActivity;
@@ -13,6 +15,8 @@ import com.android.beginnerleveljapanese.R;
 import com.android.beginnerleveljapanese.TranslateResultsActivity;
 
 import java.util.Random;
+
+import butterknife.BindView;
 
 /**
  * Implementation of App Widget functionality.
@@ -39,10 +43,13 @@ public class BeginnerJapaneseWidgetProvider extends AppWidgetProvider {
 
         PendingIntent pendingSearchablePendingIntent = PendingIntent.getActivity(context, 0, searchableActivityIntent, 0);
         PendingIntent mainActivityPendingIntent = PendingIntent.getActivity(context, 0, mainActivityIntent, 0 );
+
         views.setOnClickPendingIntent(R.id.widget_translator, mainActivityPendingIntent);
         views.setOnClickPendingIntent(R.id.widget_translator_search_icon, pendingSearchablePendingIntent);
+
         views.setTextViewText(R.id.widget_phrases_romaji_tv, randomRomajiPhrase);
         views.setTextViewText(R.id.widget_phrases_list_tv, randomEngTranslatedRomaji);
+        views.setImageViewResource(R.id.widget_favorite_image_button, R.drawable.ic_favorite);
 
 
         // Instruct the widget manager to update the widget
@@ -51,6 +58,10 @@ public class BeginnerJapaneseWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        //generate random japanese phrase onUpdate.
+        generateRandomPhrase(context);
+
         Log.d(TAG, "onUPDATE");
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
@@ -60,20 +71,29 @@ public class BeginnerJapaneseWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-
-        romajiArray = context.getResources().getStringArray(R.array.accommodation_phrases_romaji);
-        romaji_eng_translated_Array = context.getResources().getStringArray(R.array.accommodation_phrases);
-
-        int phrase_generator = new Random().nextInt(romajiArray.length);
-        randomRomajiPhrase = romajiArray[phrase_generator];
-        randomEngTranslatedRomaji = romaji_eng_translated_Array[phrase_generator];
-
+        generateRandomPhrase(context);
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    /**
+     *
+     * Helper method that uses Random Class to generate a random japanese phrase (romaji + english)
+     * from the xml string array files.
+     * @param context used to access resource xml files
+     */
+    void generateRandomPhrase(Context context){
+        //populate arrays
+        romajiArray = context.getResources().getStringArray(R.array.accommodation_phrases_romaji);
+        romaji_eng_translated_Array = context.getResources().getStringArray(R.array.accommodation_phrases);
+        //pull random phrase from arrays w/ randomly generated int
+        int phrase_generator = new Random().nextInt(romajiArray.length);
+        randomRomajiPhrase = romajiArray[phrase_generator];
+        randomEngTranslatedRomaji = romaji_eng_translated_Array[phrase_generator];
+
     }
 }
 
