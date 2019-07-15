88dec902769eca28e0a4998741f681d20349bf56
gr3y6h0st <jayvy19@gmail.com>
2019-07-14 22:10:19 -0700

    Added Katakana Tab.

--- src/main/java/com/jaydroid/beginnerleveljapanese/MainActivity.java
+++ src/main/java/com/jaydroid/beginnerleveljapanese/MainActivity.java
@@ -163,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 4;
         }
     }
 }
